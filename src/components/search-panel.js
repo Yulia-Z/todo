718f263cf03aba8e0c9f9a32023acc9daf4d20f9
Yuliya Zavarskaya <yulia@Yuliyas-MacBook-Air.local>
2019-05-18 22:41:17 -0700

    added buttons

---
+++ src/components/search-panel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input type="text"
+               className="search-input"
+               placeholder="search" />
+    )
+};
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-    return (
-        <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
-        </ul>
-    );
-};
-
-const AppHeader = () => {
-    return (
-        <h1>My Todo List</h1>
-    );
-};
-
-const SearchPanel = () => {
-    return (
-        <input placeholder="search"/>
-    )
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+import './index.css';
 
 const App = () => {
+
+    const todoData = [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awesome App', important: true, id: 2},
+        { label: 'Have a lunch', important: false, id: 3}
+    ];
+
     return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList />
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3}/>
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+
+            <TodoList todos={todoData}/>
         </div>
     );
 };
